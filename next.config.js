e06db7bdcefd2dc82720340f3e6a83fc9a5cc4b3
huyvda <vuducanhhuy@gmail.com>
2023-05-05 08:46:21 +0700

    add file .env

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        db_username: "vuducanhhuy",
-        db_password: "Zf55Y78qKpBJz8gk",
-        db_cluster: "cluster0",
+        db_username: process.env.DB_USERNAME,
+        db_password: process.env.DB_PASSWORD,
+        db_cluster: process.env.DB_CLUSTER,
         db_database: "HUY'S_BLOG_DEV",
       },
       reactStrictMode: true,
@@ -15,9 +15,9 @@ module.exports = (phase) => {
 
   return {
     env: {
-      db_username: "vuducanhhuy",
-      db_password: "Zf55Y78qKpBJz8gk",
-      db_cluster: "cluster0",
+      db_username: process.env.DB_USERNAME,
+      db_password: process.env.DB_PASSWORD,
+      db_cluster: process.env.DB_CLUSTER,
       db_database: "HUY'S_BLOG",
     },
     reactStrictMode: true,
