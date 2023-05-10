ef1a6c280b62d51c83830856d054278fc17aeb81
huyvda <vuducanhhuy@gmail.com>
2023-05-10 23:17:03 +0700

    mod. downgrade nextjs version

--- pages/index.js
+++ pages/index.js
@@ -4,37 +4,6 @@ import { getAllPosts } from "@/lib/api-utils";
 import Head from "next/head";
 import { Fragment } from "react";
 
-export const DUMMY_DATA = [
-  {
-    title: "Getting Started NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "This is section talk about basis of NextJS",
-    date: "2023-05-03",
-    slug: "getting-started-nextjs",
-  },
-  {
-    title: "Getting Started NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "This is section talk about basis of NextJS",
-    date: "2023-05-03",
-    slug: "getting-started-nextjs2",
-  },
-  {
-    title: "Getting Started NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "This is section talk about basis of NextJS",
-    date: "2023-05-03",
-    slug: "getting-started-nextjs3",
-  },
-  {
-    title: "Getting Started NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt: "This is section talk about basis of NextJS",
-    date: "2023-05-03",
-    slug: "getting-started-nextjs4",
-  },
-];
-
 export default function HomePage(props) {
   return (
     <Fragment>
